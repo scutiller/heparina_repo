4630422ac07edf81b8a1b69144c134d4842cc742
scutiller <scutiller@gmail.com>
2025-03-16 09:41:57 -0300

    solucionando problema de rutas

--- calculos/forms.py
+++ calculos/forms.py
@@ -4,10 +4,9 @@ from .models import BombaHeparina
 class BombaHeparinaForm(forms.ModelForm):
     class Meta:
         model = BombaHeparina
-        fields = ['nombre_paciente', 'peso', 'volumen', 'infusion_actual', 'kptt']
+        fields = ['peso', 'volumen', 'infusion_actual', 'kptt']
                 
         labels = {
-            'nombre_paciente': 'Nombre del Paciente',
             'peso': 'Peso del Paciente (kg)',
             'volumen': 'Volumen de Solución (ml)',
             'infusion_actual': 'Infusión Actual (ml/hr)',
